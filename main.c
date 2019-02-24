4cda5161aa5f889caf342dc16e98be0943da7c6c
tharusha-kudagala <44969527+tharusha-kudagala@users.noreply.github.com>
2019-02-24 11:21:41 +0530

    main.c
    === Added new functions === 
    + Square root
    + Power
    === Minor changes in User Interface ===

--- main.c
+++ main.c
@@ -1,43 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include<string.h>
+#include <math.h>
 
 int main()
 {
-    printf("--------------- [+ - * / ] calculator -----");
+    printf("------ calculator ------\n\nAddition \t\t[+]\nSubtraction \t\t[-]\nMultiplication \t\t[*]\nDivision \t\t[/]\nPower \t\t\t[^]\nSquare root \t\t[R] ");
 
     char operator;
     double num1,num2,result;
 
-    printf("\n\nenter the first number: ");
+    printf("\n\nEnter the first number: ");
     scanf("%lf",&num1);
 
-    printf("\nenter the operator: ");
+    printf("\nEnter the operator: ");
     scanf("%s",&operator);
 
-    printf("\nenter the second number: ");
-    scanf("%lf",&num2);
+    if(operator!='R')
+    {printf("\nEnter the second number: ");
+    scanf("%lf",&num2);}
 
     switch(operator)
     {
         case '+' :
-        printf("\nresult : %.1f",(num1 + num2));
+        printf("\nResult : %.2f\n",(num1 + num2));
         break;
 
         case '-' :
-        printf("\nresult : %.1f",(num1 - num2));
+        printf("\nResult : %.2f\n",(num1 - num2));
         break;
 
         case '*' :
-        printf("\nresult : %.1f",(num1 * num2));
+        printf("\nResult : %.2f\n",(num1 * num2));
         break;
 
         case '/' :
-        printf("\nresult : %.1f",(num1 / num2));
+        printf("\nResult : %.2f\n",(num1 / num2));
         break;
 
+        case 'R' :
+        printf("\nResult : %.3f\n",sqrt(num1));
+        break;
 
-        default : printf("option is not available :(");
+        case '^' :
+        printf("\nResult : %.2f\n",pow(num1,num2));
+        break;
+
+        default : printf("\nOption is not available :( \n");
     }
     return 0;
 }
